6b9d1dc799e71daa5cd040812ed5838031e3932d
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-20 14:46:32 -0300

    Passo 2

--- livro.js
+++ livro.js
@@ -2,7 +2,7 @@
 let livroUm = {
     titulo:'A anscenção de um Guerreiro',
     autor: 'Fax',
-    anoPublicacao: 2018,
+    anoPublicacao: 2024,
     genero: 'Biografia',
 };
 
@@ -13,3 +13,26 @@ const dadosLivro  = `Os dados do livro são :
     genero: ${livroUm.genero},
 `
 console.log(dadosLivro);
+
+const anoAtual = new Date().getFullYear();
+
+
+let livroDois = {
+    titulo:'O começo da queda e o fundo do poço',
+    autor: 'Fax',
+    anoPublicacao: 2016,
+    genero: 'Biografia',
+    
+};
+
+livroDois.idadePublicacao = anoAtual - livroDois.anoPublicacao;
+
+const dadosLivroDois  = `Os dados do livro são :
+    titulo: ${livroDois.titulo},
+    autor: ${livroDois.autor},
+    anoPublicacao: ${livroDois.anoPublicacao},
+    genero: ${livroDois.genero},
+    idadePublicacao: ${livroDois.idadePublicacao},
+`
+
+console.log(dadosLivroDois);
