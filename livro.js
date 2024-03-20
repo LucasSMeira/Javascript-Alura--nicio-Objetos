f532bd0168d2ba81dd7a16eab362490abd3aeb71
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-20 14:34:28 -0300

    Passo 1

---
+++ livro.js
@@ -0,0 +1,15 @@
+
+let livroUm = {
+    titulo:'A anscenção de um Guerreiro',
+    autor: 'Fax',
+    anoPublicacao: 2018,
+    genero: 'Biografia',
+};
+
+const dadosLivro  = `Os dados do livro são :
+    titulo: ${livroUm.titulo},
+    autor: ${livroUm.autor},
+    anoPublicacao: ${livroUm.anoPublicacao},
+    genero: ${livroUm.genero},
+`
+console.log(dadosLivro);
